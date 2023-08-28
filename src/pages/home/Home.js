b1e9fd26c6a16df5c677cc8fd4898b7732cf0d17
Sarafraj Mallick <sarafraj01999@gmail.com>
2023-08-28 10:23:54 +0530

    lazy loading fix

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import Cards from "../../components/card/Card";
 import Filter from "../../components/filter/Filter";
@@ -10,10 +10,14 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useDispatch } from "react-redux";
 import { addMovie } from "../../actions/movies";
 import { setFilterSelections, setSearchKey } from "../../actions/filter";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
+const TempArray = Array.from(Array(24).keys());
 
 const Home = () => {
   const { searchKey, filterSelections } = useSelector((state) => state.filter);
   const { moviesList } = useSelector((state) => state.movies);
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   const activeCategories = Genres.filter((s, i) => {
@@ -32,11 +36,13 @@ const Home = () => {
   useUpdateEffect(() => {
     (async () => {
       try {
+        setIsLoading(true);
         const menuItems = await filterByQueryAndCategories(
           searchKey,
           activeCategories
         );
         dispatch(addMovie(menuItems.value));
+        setIsLoading(false);
       } catch (err) {
         console.log(err);
       }
@@ -48,14 +54,30 @@ const Home = () => {
       <Filter />
       <motion.div layout animate={{ y: 30 }} className="list__cards">
         <AnimatePresence>
-          {moviesList.length !== 0 ? (
-            moviesList.map((movie, idx) => <Cards key={idx} movie={movie} />)
+          {isLoading ? (
+            <>
+              {TempArray.map((ele) => (
+                <div className="cards">
+                  <SkeletonTheme color="#202020" highlightColor="#444">
+                    <Skeleton height={300} duration={2} />
+                  </SkeletonTheme>
+                </div>
+              ))}
+            </>
           ) : (
-            <div style={{ fontSize: "1.5rem" }}>
-              <p>SearchQuery : {searchKey ? searchKey : "Empty"}</p>
-              <p>Genres : {activeCategories.join(",")}</p>
-              <p>Message : 404 Not Found !</p>
-            </div>
+            <>
+              {moviesList.length !== 0 ? (
+                moviesList.map((movie, idx) => (
+                  <Cards key={idx} movie={movie} />
+                ))
+              ) : (
+                <div style={{ fontSize: "1.5rem" }}>
+                  <p>SearchQuery : {searchKey ? searchKey : "Empty"}</p>
+                  <p>Genres : {activeCategories.join(",")}</p>
+                  <p>Message : 404 Not Found !</p>
+                </div>
+              )}
+            </>
           )}
         </AnimatePresence>
       </motion.div>
